84203f3ee2ca5eaf2b3377aaceb51e6ab6350ec9
Shelley Brook <sbrook13@gmail.com>
2020-06-27 01:15:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+
+function happyHoliday(name){
+  return `Happy holidays, ${name}!`
+}
+function happyCustomHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`
+}
