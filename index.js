afd5ea7beee3e2cfd5d014acfb273e807744b33c
Shelley Brook <sbrook13@gmail.com>
2020-06-27 01:16:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
-function happyHoliday(name){
+function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 function happyCustomHolidayTo(holiday,name){
